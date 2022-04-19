29791061aa3d9fdb7206060f9c092b979565d033
Vincent Luczkow <vincent.luczkow@gmail.com>
2022-04-18 23:53:23 -0400

    Slightly cleaner code, let's you continue the game

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Rules:
 1. Run `topology_game.py`. It will give you two topological properties A and B.
 2. Prove one of:
-    - A iff B
     - A -> B
-    - B -> A
     - A -> not B
-    - B -> not A
     - None of the above, and give counterexamples
+3. When you are finished, enter `next` (or anything but `q`) to generate a property C.
+4. Check C against A and B
+5. And so on
+6. Enter `q` to quit.
--- topology_game.py
+++ topology_game.py
@@ -3,7 +3,7 @@ import pickle
 from sys import argv
 
 
-PROPERTIES = [
+PROPERTIES = {
     'connected',
     'path connected',
     'locally connected',
@@ -28,8 +28,8 @@ PROPERTIES = [
     'bigger than R',
     'T_0',
     'T_1 (Frechet)',
-    'T_2 (Hausdorff',
-    'T_2.5 (Urysohn)'
+    'T_2 (Hausdorff)',
+    'T_2.5 (Urysohn)',
     'T_3 (regular Hausdorff',
     'T_3.5 (Tychonoff',
     'T4 (normal Hausdorff)',
@@ -81,14 +81,34 @@ PROPERTIES = [
     'Countably tight',
     'Angelic',
     # Can't read 171 or 173
-    'All subsets open xor closed',
+    'All subsets open xor closed'
     # Can't read 177 - end
-]
+}
+
+
+def prop_string(p: str) -> str:
+    p_not_p = '' if random.randrange(2) else 'not '
+    return f'{p_not_p}{p}'
 
 
 def main():
-    a, b = random.sample(PROPERTIES, 2)
-    print(f'Properties:\n    {a}\n    {b}')
+    global PROPERTIES
+    # Can't sample from a set anymore
+    a, b = random.sample(list(PROPERTIES), 2)
+    print(f'Properties:\n    {prop_string(a)}\n    {prop_string(b)}')
+    PROPERTIES -= {a, b}
+    done = False
+
+    while not done:
+        i = input('Next property? ')
+        if i == 'q':
+            done = True
+            continue
+        else:
+            c = random.sample(list(PROPERTIES), 1)[0]
+            PROPERTIES -= {c}
+            print(f'New property: {prop_string(c)}')
+
 
 
 if __name__ == "__main__":
